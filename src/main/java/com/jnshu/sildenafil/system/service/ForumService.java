e6d3f245faedf33d55abbcd8e2161eae356493ec
tme <tme1024723995@126.com>
2018-10-31 20:44:29 +0800

    铁增加了forumService的模糊查询方法

--- src/main/java/com/jnshu/sildenafil/system/service/ForumService.java
+++ src/main/java/com/jnshu/sildenafil/system/service/ForumService.java
@@ -3,6 +3,8 @@ package com.jnshu.sildenafil.system.service;
 import com.jnshu.sildenafil.system.domain.Forum;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.List;
+
 /**
  * <p>
  *  服务类
@@ -12,5 +14,5 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2018-10-31
  */
 public interface ForumService extends IService<Forum> {
-
+    public List<Forum> FormFuzzySelect(String title,String author,long start,long end);
 }
--- src/main/java/com/jnshu/sildenafil/system/service/impl/ForumServiceImpl.java
+++ src/main/java/com/jnshu/sildenafil/system/service/impl/ForumServiceImpl.java
@@ -1,10 +1,17 @@
 package com.jnshu.sildenafil.system.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.jnshu.sildenafil.system.domain.Forum;
+import com.jnshu.sildenafil.system.domain.Student;
 import com.jnshu.sildenafil.system.mapper.ForumDao;
+import com.jnshu.sildenafil.system.mapper.StudentDao;
 import com.jnshu.sildenafil.system.service.ForumService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * <p>
@@ -16,5 +23,31 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ForumServiceImpl extends ServiceImpl<ForumDao, Forum> implements ForumService {
-
+    @Autowired
+    ForumDao forumDao;
+    @Autowired
+    StudentDao studentDao;
+    @Override
+    public List<Forum> FormFuzzySelect(String title,String author,long start,long end){
+        QueryWrapper<Forum> wrapper = new QueryWrapper<>();
+        if(author!= null) {
+            QueryWrapper<Student> studentWrapper = new QueryWrapper<>();
+            studentWrapper.select("id").like("nickname", author);
+            List<Student> stuList = studentDao.selectList(studentWrapper);
+            List idList = stuList.stream().map(Student::getId).collect(Collectors.toList());
+            wrapper.in("student_id",idList);
+        }
+        if(title != null){
+            wrapper.like("title",title);
+        }
+        if(end == 0){
+            wrapper.ge("create_at",start);
+        }else if(start == 0){
+            wrapper.le("create_at",end);
+        }else{
+            wrapper.between("create_at",start,end);
+        }
+        wrapper.select("title","body","student_id","create_at");
+        return forumDao.selectList(wrapper);
+    }
 }
