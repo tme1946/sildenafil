e58d7995d13cf659d3db6c3391290b40a0d8bb1b
tme <tme1024723995@126.com>
2018-11-01 16:25:23 +0800

    铁增加了forumService的模糊查询方法

--- src/main/java/com/jnshu/sildenafil/system/service/ForumService.java
+++ src/main/java/com/jnshu/sildenafil/system/service/ForumService.java
@@ -14,5 +14,10 @@ import java.util.List;
  * @since 2018-10-31
  */
 public interface ForumService extends IService<Forum> {
+    /**
+     * 帖子多条件模糊查询 
+     * @param [title, author, start, end]
+     * @return  java.util.List<com.jnshu.sildenafil.system.domain.Forum>
+     */
     public List<Forum> FormFuzzySelect(String title,String author,long start,long end);
 }
