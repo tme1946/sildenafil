90ec75f9b7b1611f5b01e48a89ffc6f1c835b7be
yuefeifei <892499056@qq.com>
2018-11-16 19:56:33 +0800

    feifei add role

--- src/main/java/com/jnshu/sildenafil/system/domain/Module.java
+++ src/main/java/com/jnshu/sildenafil/system/domain/Module.java
@@ -3,9 +3,11 @@ package com.jnshu.sildenafil.system.domain;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.jnshu.sildenafil.common.validation.Save;
 import com.jnshu.sildenafil.common.validation.Update;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 /**
@@ -23,7 +25,7 @@ public class Module implements Serializable {
     /**
      * 模块id
      */
-    @NotBlank(message ="{module.moduleId.validation.error}" ,groups = {Update.class})
+    @NotNull(message ="{module.id.validation.error}" ,groups = {Update.class})
     @TableId(value = "id", type = IdType.AUTO)
     private Long id;
 
@@ -54,30 +56,35 @@ public class Module implements Serializable {
     /**
      * 模块名称
      */
+    @NotBlank(message ="{module.name.validation.error}" ,groups = {Update.class,Save.class})
     @TableField("name")
     private String name;
 
     /**
      * 权限标识
      */
+    @NotBlank(message ="{module.permission.validation.error}" ,groups = {Update.class,Save.class})
     @TableField("permission")
     private String permission;
 
     /**
      * 模块地址
      */
+    @NotBlank(message ="{module.url.validation.error}" ,groups = {Update.class,Save.class})
     @TableField("url")
     private String url;
 
     /**
      * 模块类型,0为菜单,1为按钮，默认为0；
      */
+    @NotNull(message ="{module.type.validation.error}" ,groups = {Update.class,Save.class})
     @TableField("type")
     private Integer type;
 
     /**
      * 父模块id
      */
+    @NotNull(message ="{module.parentId.validation.error}" ,groups = {Update.class,Save.class})
     @TableField("parent_id")
     private Long parentId;
 
