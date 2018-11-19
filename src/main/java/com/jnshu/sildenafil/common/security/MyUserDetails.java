47afb3d3aaa2fa9fe1a52f5b54b9a2a20c0bf0ef
yuefeifei <892499056@qq.com>
2018-11-19 21:10:05 +0800

    feifei update security

--- src/main/java/com/jnshu/sildenafil/common/security/MyUserDetails.java
+++ src/main/java/com/jnshu/sildenafil/common/security/MyUserDetails.java
@@ -3,10 +3,14 @@ package com.jnshu.sildenafil.common.security;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.jnshu.sildenafil.common.validation.UserLogin;
+import com.jnshu.sildenafil.common.validation.UserSave;
+import com.jnshu.sildenafil.common.validation.UserUpdate;
 import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 
+import javax.validation.constraints.Size;
 import java.util.Collection;
 import java.util.Set;
 
@@ -17,6 +21,45 @@ import java.util.Set;
 public class MyUserDetails extends User {
 
     private static final long serialVersionUID = 1L;
+    /**
+     * 用户id
+     */
+   private Long id;
+
+    /**
+     * 创建时间
+     */
+    private Long createAt;
+
+    /**
+     * 更新时间
+     */
+    private Long updateAt;
+
+    /**
+     * 创建人
+     */
+    private String createBy;
+
+    /**
+     * 更新人
+     */
+    private String updateBy;
+
+    /**
+     * 后台账户名
+     */
+    private String userName;
+
+    /**
+     * 后台账户密码
+     */
+   private String password;
+
+    /**
+     * 角色id
+     */
+    private Long roleId;
 
     public MyUserDetails(String username, String password, Collection<? extends GrantedAuthority> authorities) {
         super(username, password, authorities);
